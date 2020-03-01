b39c55260261f20e60d9ec541b0385237d56912e
Camille Vo <cvo23@ucmerced.edu>
2020-03-01 06:32:44 -0800

    Added local files

---
+++ A10M_marlin1.1.8/Marlin/camilles_test.h
@@ -0,0 +1,8 @@
+// Include the things I need to includes
+// Record number until click, whether it's up or down
+// on click, display the first digit and move to the second
+// after 4, call function that looks up the associated number in array of sd card files
+// start print.
+// M23  - Select SD file: "M23 /path/file.gco". (Requires SDSUPPORT)
+// * M24  - Start/resume SD print. (Requires SDSUPPORT)
+//
